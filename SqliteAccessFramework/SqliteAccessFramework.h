18311824828f462f4cba6d4d2045db345862c782
Aldo Dell <aldodell@macbookaldo.local>
2016-10-27 22:17:01 -0400

    Initial Commit

---
+++ SqliteAccessFramework/SqliteAccessFramework.h
@@ -0,0 +1,19 @@
+//
+//  SqliteAccessFramework.h
+//  SqliteAccessFramework
+//
+//  Created by Aldo Dell on 27/10/16.
+//  Copyright © 2016 Aldo Dell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SqliteAccessFramework.
+FOUNDATION_EXPORT double SqliteAccessFrameworkVersionNumber;
+
+//! Project version string for SqliteAccessFramework.
+FOUNDATION_EXPORT const unsigned char SqliteAccessFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SqliteAccessFramework/PublicHeader.h>
+
+
